59421635a69172bab57c5f47c3c9e365636e8747
mpdevDotDev <marcelo.pastore@mpdev.dev>
2022-01-05 21:41:43 -0300

    created examples structure

--- src/example/index.tsx
+++ src/example/index.tsx
@@ -1,11 +1,43 @@
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-// import { App } from "./App";
+import cases from "./cases";
 
-const App = () => {
-  return <div>Teste</div>;
+const casesDict = {
+  basic: <cases.Basic />,
 };
 
+const Menu = ({ onClick }: { onClick: (string) => void }) => (
+  Object.keys(casesDict).map((slug) => {
+    return (
+      <li key={`cases-${slug}`}>
+        <a
+          href={slug}
+          onClick={(e) => {
+            e.preventDefault();
+            onClick(slug)
+          }}
+        >
+          {slug}
+        </a> 
+      </li>
+    );
+  })
+);
+
+const App = () => {
+  const [actExample, setActExample] = useState();
+
+  return (
+    <>
+      <ul><Menu onClick={setActExample}/></ul>
+      <div>
+        {actExample && (
+          casesDict[actExample]
+        )}
+      </div>
+    </>
+  );
+};
 
 const app = document.getElementById("app");
-console.log(app);
 ReactDOM.render(<App />, app);
