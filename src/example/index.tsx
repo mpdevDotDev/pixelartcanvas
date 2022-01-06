fec8cfed025f45d82bea1d6ad88bbd42e8c5a946
mpdevDotDev <marcelo.pastore@mpdev.dev>
2022-01-05 21:07:57 -0300

    initial commit

---
+++ src/example/index.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom";
+// import { App } from "./App";
+
+const App = () => {
+  return <div>Teste</div>;
+};
+
+
+const app = document.getElementById("app");
+console.log(app);
+ReactDOM.render(<App />, app);
